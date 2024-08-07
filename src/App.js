bf168ae19176e78fcfd549d7d32c6089cfaf35f4
mohammadhosseinrajabi <shayanj9999@gmail.com>
2024-08-07 20:04:17 +0330

    register ezafe shood

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Login from './component/login';
+import Register from './component/register';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+      <Register/>
     </div>
   );
 }
--- src/component/Formikcomponent/formikControl.jsx
+++ src/component/Formikcomponent/formikControl.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Input from './input';
+import Radio from './radioButton';
+import Date from './date';
+import File from './file';
 
  const FormikControl=(props)=> {
 
         switch (props.control) {
             case 'input':
                 return <Input {...props}/>
-                break;
-        
+              
+            case 'radio':
+            return <Radio {...props}/>
+
+            case 'date':
+                return <Date {...props}/>
+            case 'file':
+                return <File {...props}/>
+           
             default:
                 break;
         }
